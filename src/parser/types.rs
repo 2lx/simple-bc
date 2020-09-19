6f967222b463b61214f801339cd437ba3a5a3afd
Alexey Minchakov <lexaaim@gmail.com>
2020-09-20 00:51:07 +0300

    add UnaryMinus operation

--- src/parser/types.rs
+++ src/parser/types.rs
@@ -3,12 +3,12 @@ use std::fmt;
 #[derive(Debug)]
 pub struct Loc(pub usize, pub usize);
 
-pub struct Block(pub Vec<Expr>);
+pub struct Block(pub Expr);
 
 impl fmt::Debug for Block {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let Block(exprs) = self;
-        write!(f, "{:?}", exprs)
+        write!(f, "[{:?}]", exprs)
     }
 }
 
@@ -18,6 +18,7 @@ pub enum Expr {
     Add(Loc, Box<Expr>, Box<Expr>),
     Subtract(Loc, Box<Expr>, Box<Expr>),
     StringLiteral(Loc, std::string::String),
+    UnaryMinus(Loc, Box<Expr>),
 }
 
 impl fmt::Debug for Expr {
@@ -29,6 +30,7 @@ impl fmt::Debug for Expr {
             Add(_, l, r) => write!(f, "({:?} + {:?})", l, r),
             Subtract(_, l, r) => write!(f, "({:?} - {:?})", l, r),
             StringLiteral(_, s) => write!(f, "{}", s),
+            UnaryMinus(_, r) => write!(f, "-{:?}", r),
         }
     }
 }
